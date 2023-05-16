7836dd4cf15f065b735f71697e9387d6f8095ada
Marek Michali <marek.michali@sap.com>
2023-05-16 11:22:47 +0200

    wip

--- scripts/create_changelog.sh
+++ scripts/create_changelog.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LATEST_RELEASE=0.4.0 # for testability
+LATEST_RELEASE="" # for testability
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
