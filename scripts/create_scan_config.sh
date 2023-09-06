2ad780e9cf30cc1d3d727324f119eade515df748
Marek Michali <marek.michali@sap.com>
2023-09-06 12:06:30 +0200

    Optionable

--- scripts/create_scan_config.sh
+++ scripts/create_scan_config.sh
@@ -6,6 +6,7 @@
 # ./create_scan_config image temp_scan_config.yaml
 
 FILENAME=${2-../sec-scanners-config.yaml}
+TAG=${3:-}
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
@@ -13,16 +14,20 @@ set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being maskedPORT=5001
 
-TAG=${1}
+IMAGE=${1}
 echo "Creating security scan configuration file:"
 cat <<EOF | tee ${FILENAME}
 module-name: btp-operator
-rc-tag: ${TAG}
 protecode:
-  - ${IMAGE_REPO}:${TAG}
+  - ${IMAGE}
 whitesource:
   language: golang-mod
   subprojects: false
   exclude:
     - "**/*_test.go"
 EOF
+
+# add rc-tag: ${TAG} when creating the config for main
+if [ -n "${TAG}" ]; then
+  sed -i "s/protecode:/rc-tag: ${TAG}\nprotecode:/" "${FILENAME}"
+fi
